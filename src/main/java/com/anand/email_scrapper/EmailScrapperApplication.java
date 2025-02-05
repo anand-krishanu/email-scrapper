bbff997fff3daf0042e9da174835a15587628bf9
anand-krishanu <anandkrishanu26@gmail.com>
2025-02-05 12:53:25 +0530

    initial commit

---
+++ src/main/java/com/anand/email_scrapper/EmailScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.anand.email_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anand/email_scrapper/EmailScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.email_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
