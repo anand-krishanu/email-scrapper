b8f15276beca4510776b353ff063143b09051429
anand-krishanu <anandkrishanu26@gmail.com>
2025-02-06 17:53:29 +0530

    Controller and main

--- src/main/java/com/anand/email_scrapper/EmailScrapperApplication.java
+++ src/main/java/com/anand/email_scrapper/EmailScrapperApplication.java
@@ -1,5 +1,6 @@
 package com.anand.email_scrapper;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailScrapperApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+
 		SpringApplication.run(EmailScrapperApplication.class, args);
 	}
 
---
+++ src/main/java/com/anand/email_scrapper/controller/EmailController.java
@@ -0,0 +1,46 @@
+package com.anand.email_scrapper.controller;
+
+import com.anand.email_scrapper.entity.Email;
+import com.anand.email_scrapper.repository.EmailRepository;
+import com.anand.email_scrapper.service.EmailSendingService;
+import com.anand.email_scrapper.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    EmailSendingService sendingService;
+
+    @PostMapping("/scrape")
+    public String scrape (@RequestParam String url, @RequestParam String company) {
+        emailService.scrapEmails(url, company);
+        return "Scraping started for: " + company;
+    }
+    @GetMapping("/sendAllEmails")
+    public void sendEmailsToAll() {
+        List<Email> emails = emailRepository.findAll();
+        for (Email email : emails) {
+            sendingService.sendEmail(email.getEmail(), "Test Subject", "Test Body");
+        }
+    }
+
+    @GetMapping("/getAllEmails")
+    public List<Email> getAllEmails () {
+        return emailRepository.findAll();
+    }
+
+    @PostMapping("sendEmails")
+    public void sendEmail (@RequestParam String receiver, @RequestParam String subject, @RequestParam String body) {
+        sendingService.sendEmail(receiver, subject, body);
+    }
+}
